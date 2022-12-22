2384cf5bf19381301647d64cd076973af278bdaa
01one <84787013+01one@users.noreply.github.com>
2022-12-22 16:37:03 +0600

    Update txtPad.py

--- txtPad.py
+++ txtPad.py
@@ -4,19 +4,16 @@
 import pickle
 import pygame,sys,time
 from pygame.locals import*
-import render
 pygame.init()
 clock=pygame.time.Clock()
 then=time.time()
 
 pulse='|'
 try:
-	#Read Data
 	with open('TextData.pickle', 'rb') as data:
 		all_data= pickle.load(data)
 		data.close()
 except:
-	#Write Data
 	with open('TextData.pickle', 'wb') as data:
 		txt=''
 		font_size=30
@@ -31,10 +28,10 @@ txt=all_data[0]
 font_size=all_data[1]
 w=1000
 h=650
-#screen=pygame.display.set_mode((w,h),RESIZABLE)
 screen=pygame.display.set_mode((w,h),FULLSCREEN|HWSURFACE)
 pygame.display.set_caption("txtPad")
-#pygame.mouse.set_visible(1)
+icon=pygame.image.load('txtPad.png')
+pygame.display.set_icon(icon)
 writing=True
 
 
@@ -42,6 +39,41 @@ TOTAL_PAGE_NUMBER=1
 CURRENT_PAGE=TOTAL_PAGE_NUMBER
 RENDER_LINE_HEIGHT=100
 START_LINE=1
+
+text_font=pygame.font.Font('Prata-Regular.ttf',font_size)
+
+def render_text_list(text,t_w,text_font):
+	text_lines=[]
+	splitted_lines=text.splitlines()
+	for splitted_line in splitted_lines:
+		if text_font.size(splitted_line)[0] >t_w:
+			words = splitted_line.split(' ')
+			fitted_line=""
+			for word in words:
+				test_line = fitted_line + word + " "
+				if text_font.size(test_line)[0] <t_w:
+					fitted_line = test_line
+				else:
+					text_lines.append(fitted_line)
+					fitted_line = word + " "
+			text_lines.append(fitted_line)
+		else:
+			text_lines.append(splitted_line)
+	return text_lines
+
+
+def render(screen,render_lines,text_font,t_x,t_y,text_color,all_select):
+	for line in render_lines:
+		if line != "":
+			text_surface = text_font.render(line, 1, text_color)
+			if all_select==True:
+				txt_rect=text_surface.get_rect()
+				txt_rect=(t_x,t_y,txt_rect[2],txt_rect[3])
+				pygame.draw.rect(screen,"#ccffcc",txt_rect)
+			screen.blit(text_surface, (t_x, t_y))
+		t_y +=text_font.size(line)[1]
+
+
 class TextView():
 	def __init__(self,screen,text='',t_x=0,t_y=0,t_w=200,t_h=400,text_color="#666666",pulse=''):
 		self.screen=screen
@@ -52,7 +84,7 @@ class TextView():
 		self.text_color=text_color 
 		self.text=text+pulse
 		self.text_font=pygame.font.Font('Prata-Regular.ttf',font_size)
-		self.text_lines=render.render_text_list(self.text,t_w,self.text_font)
+		self.text_lines=render_text_list(self.text,t_w,self.text_font)
 
 		global CURRENT_PAGE,TOTAL_PAGE_NUMBER,RENDER_LINE_HEIGHT,START_LINE
 			
@@ -77,7 +109,7 @@ class TextView():
 		END_LINE=CURRENT_PAGE*EACH_PAGE_CAPACITY
 		render_lines=self.text_lines[START_LINE-1:END_LINE]
 		RENDER_LINE_HEIGHT=text_line_height*len(render_lines)
-		render.render_lines(self.screen,render_lines,self.text_font,self.t_x,self.t_y,self.text_color,all_select)
+		render(self.screen,render_lines,self.text_font,self.t_x,self.t_y,self.text_color,all_select)
 		
 
 all_select=False
@@ -110,7 +142,6 @@ while game_running:
 			
 		if event.type==pygame.KEYDOWN:
 			if event.key==pygame.K_BACKSPACE:
-				#CURRENT_PAGE=TOTAL_PAGE_NUMBER
 				pygame.key.set_repeat(200,5)
 				if len(txt)==0:
 					pass
@@ -119,6 +150,7 @@ while game_running:
 				if all_select==True:
 					txt=''
 					all_select=False
+				CURRENT_PAGE=TOTAL_PAGE_NUMBER
 			if event.key==pygame.K_ESCAPE:
 				with open('TextData.pickle', 'wb') as data:
 					updateData=[]
@@ -191,5 +223,5 @@ while game_running:
 		Next_line=False
 	screen.fill('#242424')
 	pygame.draw.rect(screen,"#000000",(0,0,w,RENDER_LINE_HEIGHT+100))
-	TextView(screen,text=txt,t_x=20,t_y=10,t_w=w-50,t_h=RENDER_LINE_HEIGHT+100,pulse=pulse,text_color='#696969')
+	TextView(screen,txt,t_x=20,t_y=10,t_w=w-50,t_h=RENDER_LINE_HEIGHT+100,pulse=pulse,text_color='#696969')
 	pygame.display.update()
